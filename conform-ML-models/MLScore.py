63fd01dc21f884c2f5d9acb1202690986760d30a
arhum <arhumzafar@Arhums-Air.attlocal.net>
2021-10-16 22:14:44 -0500

    enter ML models HERE

---
+++ conform-ML-models/MLScore.py
@@ -0,0 +1,34 @@
+##Arhum Zafar -- delete
+## TODO: Add bash_operator and file sensor when Model has conformed to DAG.
+
+
+"""
+We define a class that integrates with the Seldon core interface for the ML Model.
+"""
+
+
+class MLScore:
+    """
+    Model template. You can load your model parameters in __init__ from
+    a location accessible at runtime
+    """
+
+    def __init__(self):
+        """
+        Load models and add any initialization parameters (these will
+        be passed at runtime from the graph definition parameters
+        defined in your seldondeployment kubernetes resource manifest).
+        """
+        print("Initializing")
+
+    def predict(self, X, features_names):
+        """
+        Return a prediction.
+
+        Parameters
+        ----------
+        X : array-like
+        feature_names : array of feature names (optional)
+        """
+        print("Predict called - will run identity function")
+        return X
